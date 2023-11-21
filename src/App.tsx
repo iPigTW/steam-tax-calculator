8d12f0a32a59a5e3fcc04c0959743e93536a555b
ipigtw <diegogamingtw@gmail.com>
2023-11-21 15:22:51 +0800

    fixed bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
 function App() {
-  const [price, setPrice] = useState(0);
-  const [newPrice, setNewPrice] = useState(0);
+  const [price, setPrice] = useState<any | null>(0);
+  const [newPrice, setNewPrice] = useState<any | null>(0);
   return (
     <>
       <div className='container'>
