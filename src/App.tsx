7daeaa75ee4346d12e22a4ce82bf6d1d6d7026d9
ipigtw <diegogamingtw@gmail.com>
2023-11-21 20:28:52 +0800

    added placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 function App() {
-  const [price, setPrice] = useState<any | null>(0);
-  const [newPrice, setNewPrice] = useState<any | null>(0);
+  const [price, setPrice] = useState<any | null>(null);
+  const [newPrice, setNewPrice] = useState<any | null>(null);
   return (
     <>
       <div className='container'>
         <h1>Steam Tax Calculator</h1>
-        <input type='number' onChange={e => setPrice(e.target.value)} placeholder='Price here' ></input>
+        <input type='number' onChange={e => setPrice(e.target.value)} placeholder='Before Tax Price' ></input>
         <br/>
-        <input type="number" value={newPrice} onChange={e => setNewPrice(e.target.value)}></input>
+        <input type="number" placeholder='After Tax Price' value={newPrice} onChange={e => setNewPrice(e.target.value)}></input>
         <br/>
         <button onClick={() => setNewPrice((price/1.15-0.01).toFixed(2)) }>Calculate!</button>
       </div>
