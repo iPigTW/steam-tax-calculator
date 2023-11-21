947b2804e40d964709f175841def52daf3f24c73
ipigtw <diegogamingtw@gmail.com>
2023-11-21 15:14:45 +0800

    made it :D

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [price, setPrice] = useState(0);
+  const [newPrice, setNewPrice] = useState(0);
+  return (
+    <>
+      <div className='container'>
+        <h1>Steam Tax Calculator</h1>
+        <input type='number' onChange={e => setPrice(e.target.value)} placeholder='Price here' ></input>
+        <br/>
+        <input type="number" value={newPrice} disabled></input>
+        <br/>
+        <button onClick={() => setNewPrice((price/1.15-0.01).toFixed(2)) }>Calculate!</button>
+      </div>
+      <div className='footer'>
+        <p>Made by iPig</p>
+        <p>Not affiliated with Valve Corporation</p>
+      </div>
+    </>
+  )
+}
+
+export default App
